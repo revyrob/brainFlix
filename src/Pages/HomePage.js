ef7dfb4616150ace6431b4273643a588be00cc46
revyrob <kayle.robson@gmail.com>
2022-08-25 17:26:02 -0700

    styling is similar now i need to check all spacing and sizing with the mockup

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -15,6 +15,7 @@ function HomePage() {
     const urlVids = `https://project-2-api.herokuapp.com/videos/`;
     const apiKey = `?api_key=be97841f-2e0f-41ab-8584-cf3c3e4b26a9/`;
     
+    //declare videoId with useParams()
     const {videoId} = useParams();
     
     //get the list for the videos
@@ -23,14 +24,6 @@ function HomePage() {
         .get(`${urlVids}${apiKey}`)
         .then((response) => {
             setVideoList(response.data);
-            //make an if statement here if time
-            //if(videoId === undefined) {
-            //     axios
-            //     .get(`${urlVids}84e96018-4022-434e-80bf-000ce4cd12b8${apiKey}`)
-            //     .then((response) => {
-            //     setMainVideo(response.data);
-            // })
-            //}
         })
         .catch((err) => console.log(err));
     }, []);
@@ -46,7 +39,6 @@ function HomePage() {
             })
             .catch((err) => console.log(err));
             //else statement to currently hardcode the videoId since this useEffect block is running bfore the setVideoList
-            //can try to solve after reponsive design is complete
         } else {
             axios
             .get(`${urlVids}84e96018-4022-434e-80bf-000ce4cd12b8${apiKey}`)
@@ -56,7 +48,32 @@ function HomePage() {
             .catch((err) => console.log(err));
         }
     }, [videoId]);
-
+    
+    /*
+    *Loading when pressed Links back to Homepage
+    *start State in true so that we can 
+    *toggle between loading
+    */
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+      // Loading function to load data or 
+      // fake it using setTimeout;
+      const loadData = async () => {
+        // wait for 2 secs if there is no wait
+        await new Promise((resolved) => setTimeout(resolved, 2000));
+        // set the toggle loading state
+        setLoading((loading) => !loading);
+      };
+      loadData();
+    }, [])
+      
+    // If page is in loading state, display 
+    // loading div which is a spinning circle. 
+    if (loading) {
+        return <div className='loader'></div>
+    } 
+    // If page is not in loading state, display page.
+    else {
     //need to have the && statements for each item since this code will run before the useEffects and if they
     //run first they will not have the requirements for the states, therefore there has to be the truthy statements
     //so the code runs properly
@@ -66,14 +83,13 @@ function HomePage() {
         <section className='main'>
         <section className='leftMain'>
         {mainVideo && <MainArticle item={mainVideo}/>}
-        
         {mainVideo &&<CommentArea item={mainVideo}/>}
-        
         </section>
         {mainVideo && <ArticleList itemMain={mainVideo} videoList={videoList}/>}
         </section>
         </>
         );
     }
+}
     
     export default HomePage;
--- src/components/UploadForm/UploadForm.js
+++ src/components/UploadForm/UploadForm.js
@@ -1,7 +1,7 @@
 import './UploadForm.scss';
 import Button from '../Button/Button';
-import publish from '../../assets/Icons/publish.svg';
 import { Link } from 'react-router-dom';
+import publish from '../../assets/Icons/publish.svg';
 
 function UploadForm () {
     //Link the submit with the main page with a setTimeout
@@ -15,8 +15,9 @@ function UploadForm () {
         <label className='uploadForm__label'>Add a video description
         <textarea type="text" placeholder='Add a description to your video' name='description' className='uploadForm__textarea'></textarea>
         </label>
+        <div className='grey-line'></div>
         <div className='uploadForm__btn--div'>
-        <Link to={`/HomePage/:videoId`}> 
+        <Link to={`/`}> 
         <Button className="uploadForm__btn--publish" icon={publish} alt="upload" text="PUBLISH" type="submit"/>
         </Link>
         <Button className="uploadForm__btn--cancel" text="CANCEL" type="cancel"/>
